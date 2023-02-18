3db38d3bb8a369e005d1a67d57e1241fff222008
Darwin <darwinvillaruel2215@gmail.com>
2023-02-18 18:02:50 +0800

    useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
-import CampsitesList from './features/campsites/CampsitesList';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <CampsitesList />
+      <CampsitesDirectoryPage />
       <Footer />
     </div>
   );
