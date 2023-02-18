72baf7ce718e4a656b509c5dfc45d67acd8494fc
Darwin <darwinvillaruel2215@gmail.com>
2023-02-18 20:01:39 +0800

    Header and Footer Links

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { Route, Routes } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
@@ -8,7 +11,20 @@ function App() {
   return (
     <div className='App'>
       <Header />
-      <CampsitesDirectoryPage />
+      <Routes>
+        <Route
+          path='/'
+          element={<HomePage />}
+        />
+        <Route
+          path='contact'
+          element={<ContactPage />}
+        />
+        <Route
+          path='directory'
+          element={<CampsitesDirectoryPage />}
+        />
+      </Routes>
       <Footer />
     </div>
   );
